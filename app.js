23ec8c5723b8f0a0ad9164c664c0759c74460b2c
sebastinez <sebastinez@me.com>
2020-12-08 23:02:34 -0300

    feat: creation and clearance of web3 wallet

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const web3 = new Web3()
 
+window.addEventListener("DOMContentLoaded",()=> {
+})
+
 function createWallet() {
-    web3.eth.accounts.wallet.clear()
     const seed = document.querySelector("#seed")
-    const {address,privateKey} = web3.eth.accounts.wallet.create(1,seed.value)[0]
-    addToLog("#address", address)
-    addToLog("#privateKey", privateKey)
+    web3.eth.accounts.wallet.create(1,seed.value)
+    const accounts = getWallet()
+    document.querySelector("#addresses").innerHTML = accounts
 }
 
-function addToLog(element, text){
-    console.log(text);
-    document.querySelector(element).value=text
+function clearWallet() {
+    web3.eth.accounts.wallet.clear()
+    const accounts = getWallet()
+    document.querySelector("#addresses").innerHTML = accounts
+}
+
+function getWallet() {
+    const wallet = web3.eth.accounts.wallet
+    const accounts = [];
+    for (const key in wallet) {
+        if(key.match("^[0-9]+$")) accounts.push(wallet[key])
+    }
+    return accounts.map((m,i)=>`Address${i}: ${m.address}, PrivKey${i}: ${m.privateKey}<br/>`)
+}
+
+function addToLog(element, value){
+    document.querySelector(element).value=value
 }
